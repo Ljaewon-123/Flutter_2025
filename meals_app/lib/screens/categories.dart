886867c59cd9fc38e87cf057cb753ecd3d07e367
Ljaewon-123 <jml6388@naver.com>
2025-06-03 22:34:05 +0900

    meals_app-1

---
+++ meals_app/lib/screens/categories.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CatetoriesScreen extends StatelessWidget {
+  const CatetoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        // 그리드 레이아웃 지정
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories) 
+            CategoryGridItem(category: category)
+        ]
+      )
+    );
+  }
+}
